002fdd97a89adae43b4c60f78d02d3487fd5d746
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-11 02:27:29 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+first_number = 21
+second_number = 19
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
